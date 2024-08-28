9d9a4021756861b5a94fe95a82f116ba5a87dd3f
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-08-28 18:30:44 -0300

    feat: implement script to create mock clients

--- populate.py
+++ populate.py
@@ -1,4 +1,6 @@
 import sqlite3
+import random
+import time
 
 import constants as const
 
@@ -11,16 +13,34 @@ def main():
 
     create_menu_items(cur)
 
+    create_mock_clients(cur, 1200)
+
     con.commit()
     con.close()
-
-    # new_con.close()
 
 
 def create_menu_items(cur):
     cur.executemany(
         "INSERT INTO item VALUES(:id, :nome, :categoria, :preço, :ativo)",
         const.MENU_ITEMS)
+
+
+def create_mock_clients(cur, n):
+    clients = []
+    birth_start = time.mktime(time.strptime("1945-01-01", "%Y-%m-%d"))
+    birth_end = time.mktime(time.strptime("2006-12-31", "%Y-%m-%d"))
+    reg_start = time.mktime(time.strptime("2020-01-01", "%Y-%m-%d"))
+    reg_end = time.mktime(time.strptime("2023-12-31", "%Y-%m-%d"))
+
+    for i in range(n):
+        cli = {'id': None}
+        cli['telefone'] = str(i)
+        cli['nome'] = random.choice(const.NAMES)
+        cli['aniversario'] = random.randint(int(birth_start), int(birth_end))
+        cli['data_de_registro'] = random.randint(int(reg_start), int(reg_end))
+        clients.append(cli)
+    cur.executemany("INSERT INTO cliente VALUES(:id, :telefone, :nome, \
+                    :aniversario, :data_de_registro)", clients)
 
 
 if __name__ == '__main__':
